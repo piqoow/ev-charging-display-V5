249e7bc5cc6de2dc07ec9d283ab7144d705dd9be
Rofiq Rifiansyah <piqsworkmail@gmail.com>
2024-10-06 12:43:34 +0700

    rubah koneksi ke connection.php

--- getvideo.php
+++ getvideo.php
@@ -1,18 +1,13 @@
 <?php
-$servername = "localhost";
-$username = "smartpay";
-$password = "smartpay@DEV";
-$dbname = "ev_charging_db";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
-}
+require_once('../db/connection.php'); // Include the connection file
 
 $id = $_GET['id'];
-$sql = "SELECT video_iklan FROM evgate WHERE customer_id = '$id'";
-$result = $conn->query($sql);
+
+// Use prepared statements to prevent SQL injection
+$sql = $conn->prepare("SELECT video_iklan FROM evgate WHERE customer_id = ?");
+$sql->bind_param("s", $id);
+$sql->execute();
+$result = $sql->get_result();
 
 if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
@@ -21,5 +16,9 @@ if ($result->num_rows > 0) {
     $videoUrl = ''; 
 }
 
+$sql->close();
 $conn->close();
+
+// Output the video URL (optional)
+echo json_encode(array("videoUrl" => $videoUrl));
 ?>
